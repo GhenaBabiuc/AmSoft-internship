3903081a89c5e0c6f9acb00b7cc7d2089279d5a7
Babiuc Ghenadie <babiuc.ghenadii@gmail.com>
2022-12-22 12:32:36 +0200

    test

--- src/com/inrenship/oop/Test.java
+++ src/com/inrenship/oop/Test.java
@@ -5,7 +5,7 @@ import com.inrenship.oop.service.impl.StudentServiceImpl;
 
 import java.util.Arrays;
 
-public class test {
+public class Test {
     public static void main(String[] args) {
         Student student1 = new Student(1L,"Ion","Balti", Arrays.asList(6,8,9));
         Student student2 = new Student(2L,"Gheotghe","Chisinau", Arrays.asList(8,8,9));
